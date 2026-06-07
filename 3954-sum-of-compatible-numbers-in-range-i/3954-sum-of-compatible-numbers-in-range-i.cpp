751f4c0ab5c28812e915dcf7ed5507753f3f7ff2
trinesh12 <karnetrinesh2006@gmail.com>
2026-06-07 12:54:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.48%) - LeetHub

---
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfGoodIntegers(int n, int k) {
+        int sum = 0;
+
+        int start = max(1, n - k);
+        int end = n + k;
+
+        for(int x = start; x <= end; x++) {
+            if((n & x) == 0) {
+                sum += x;
+            }
+        }
+
+        return sum;
+    }
+};
