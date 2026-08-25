df946230f65c1b47d1ba5d91abc88c88d9c12a02
trinesh12 <karnetrinesh2006@gmail.com>
2026-08-25 23:21:26 +0530

    Time: 0 ms (100%), Space: 24.2 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_set<int>hs;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]%k==0)hs.insert(nums[i]);
-        }
-        bool found=false;
-        int ans,el=k;
-       for(int i=0;i<hs.size();i++)
-       {
-        if(hs.find(el)==hs.end())
-        {
-            return el;
-        }
-        el=k*(i+2);     
-       }
-       return el;
+        vector<bool> freq(101,0);
 
+        for(int i=0;i<nums.size();i++)
+        freq[nums[i]] = 1;
 
-        
-        
-       
+        int i = k;
+        while(i <= 100) {
+            if(!freq[i])
+            return i;
+            i += k;
+        }
+        return ((100 / k) + 1) * k;
     }
 };
